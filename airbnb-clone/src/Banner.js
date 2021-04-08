f696bb366127bff80986c599b2ad45c3b6ff8b42
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-08 20:27:02 +0530

    ADD: Search/ Date Range Picker

--- airbnb-clone/src/Banner.js
+++ airbnb-clone/src/Banner.js
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Banner.css'
 import Button from '@material-ui/core/Button';
+import Search from './Search';
 
 function Banner() {
+    const [showSearch, setShowSearch] = useState(false)
+
     return (
         <div className='banner'>
+
+            <div className='banner__search' > 
+
+               {showSearch && <Search /> }
+
+
+                <Button
+                 onClick={() => setShowSearch(!showSearch)}
+                 className='banner__searchButton'
+                 variant='outlined'
+                >
+                    {showSearch ? 'Hide' : 'Search Dates'}
+                </Button>
+            </div>
             <div className='banner__info'>
                 <h1>Get out and strech your imagination.</h1>
 
