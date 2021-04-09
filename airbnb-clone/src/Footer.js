349a3c02e7366d34403c5b620e965af011147f7b
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-10 00:31:24 +0530

    ADD: Footer CSS

--- airbnb-clone/src/Footer.js
+++ airbnb-clone/src/Footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Footer.css'
 
 function Footer() {
     return (
         <div className='footer'>
-            <p>©2021 Airbnb Clone, No rights reserved, This is a demo.</p>
-            <p>·Privacy·Terms·Sitemap·Company details</p>
+            <p>©2021 Airbnb Clone, No rights reserved, This is a demo ❤️.</p>
+            <p>· Privacy · Terms · Sitemap · Company details</p>
         </div>
     )
 }
